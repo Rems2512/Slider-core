360448fa68fb37020683fe712d9bd88f33150583
DarkTJ <tjmilter@gmx.de>
2019-10-17 17:23:13 +0200

    WebserverFail1

---
+++ data/test.js
@@ -0,0 +1,32 @@
+function openAlert() {
+    window.alert("Hello World");
+}
+
+function sendData(led) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("LEDState").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "setLED?LEDstate=" + led, true);
+    xhttp.send();
+}
+
+setInterval(function () {
+    // Call a function repetatively with 2 Second interval
+    getData();
+}, 2000);
+
+function getData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("ADCValue").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "readADC", true);
+    xhttp.send();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "WiFi.h"
 #include "SPIFFS.h"
 #include "ESPAsyncWebServer.h"
+#include "FS.h" 
  
 const char* ssid = "Kamera-Slider";
 const char* password =  "123456789";
+
+#define LED 2 
  
 AsyncWebServer server(80);
  
@@ -26,13 +29,17 @@ void setup(){
  
   Serial.println(WiFi.localIP());
  
-  server.on("/dashboard", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/dashboard.html", "text/html");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
   });
  
-  server.on("/dashboard.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/dashboard.css", "text/css");
+ * server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.css", "text/css");
   });
+
+  server.on("/test.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/test.js", "text/javascript");
+});
 
   Serial.println("ServerBeginn");
  
@@ -48,4 +55,22 @@ void loop(){
   
 
 
+}
+
+void handleLED() { //Fake LED
+ String ledState = "OFF";
+ String t_state = server.argv("LEDstate"); //Refer  xhttp.open("GET", "setLED?LEDstate="+led, true);
+ Serial.println(t_state);
+ if(t_state == "1")
+ {
+  digitalWrite(LED,LOW); //LED ON
+  ledState = "ON"; //Feedback parameter
+ }
+ else
+ {
+  digitalWrite(LED,HIGH); //LED OFF
+  ledState = "OFF"; //Feedback parameter  
+ }
+ 
+ server.send(200, "text/plane", ledState); //Send web page
 }
