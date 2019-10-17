402a78ff7c392ae93ec22bdd961036740b448891
DarkTJ <tjmilter@gmx.de>
2019-10-17 10:09:44 +0200

    mastercommit

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include "WiFi.h"
+#include "SPIFFS.h"
+#include "ESPAsyncWebServer.h"
+ 
+const char* ssid = "Milter";
+const char* password =  "52275829966430474895";
+ 
+AsyncWebServer server(80);
+ 
+void setup(){
+  Serial.begin(115200);
+ 
+  if(!SPIFFS.begin()){
+     Serial.println("An Error has occurred while mounting SPIFFS");
+     return;
+  }
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  server.on("/dashboard", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/dashboard.html", "text/html");
+  });
+ 
+  server.on("/dashboard.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/dashboard.css", "text/css");
+  });
+
+  Serial.println("ServerBeginn");
+ 
+  server.begin();
+
+  Serial.println("ServerBEginnDonE");
+}
+ 
+void loop(){
+
+  
+
+
+}
